3a1972a6bb22f42814e35b0eea7c978ed8ccf801
gutchess-k <kgutchess@usgs.gov>
2024-10-17 15:42:40 -0400

    Add files via upload
    initial commit

---
+++ setup.R
@@ -0,0 +1,25 @@
+
+
+packageLoad <- 
+     function(x) {
+          for (i in 1:length(x)) {
+               if (!x[i] %in% installed.packages()) {
+                    install.packages(x[i])
+               }
+          library(x[i], character.only = TRUE)
+               }
+     }
+
+
+#    Create a string of package names
+packages <- c("tidyverse",
+              "palmerpenguins", 
+              "sf",
+              "terra", 
+              "tmap",
+              "rmarkdown", 
+              "tigris", 
+              "elevatr", 
+              "rgdal")
+
+packageLoad(packages)
